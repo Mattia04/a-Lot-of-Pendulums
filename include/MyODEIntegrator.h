40030d600327e45a07d9a8ff0e3408f694aa2583
Mattia04 <mattia.ballico@gmail.com>
2025-01-16 15:07:26 +0100

    added abstract class for ODEs integrators

---
+++ include/MyODEIntegrator.h
@@ -0,0 +1,24 @@
+//
+// Created by Mattia Ballico on 15/01/25.
+//
+
+#ifndef ODEINTEGRATOR_H
+#define ODEINTEGRATOR_H
+
+#include <armadillo>
+#include <functional>
+
+class MyODEIntegrator {
+public:
+	MyODEIntegrator() = default;
+  	virtual ~MyODEIntegrator() = default;
+
+    virtual std::vector<float> step(const float &h,
+                    const std::vector<float> &x,
+                    std::function<std::vector<float>(float, std::vector<float>, std::vector<float>)> &f)
+		const = 0;
+};
+
+
+
+#endif //ODEINTEGRATOR_H
