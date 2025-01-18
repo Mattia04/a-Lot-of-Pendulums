8eb619ccced43b9aad0f16cb8bbe3c0fd3e47f2f
Mattia04 <mattia.ballico@gmail.com>
2025-01-18 10:55:07 +0100

    Changed to armadillo's vectors, corrected computations, and added more optimizations.

--- source/MyDoublePendulum.cpp
+++ source/MyDoublePendulum.cpp
@@ -4,19 +4,21 @@
 
 #include "MyDoublePendulum.h"
 
-std::vector<float> MySimpleDoublePendulum::eval(const std::vector<float>& x) const
+arma::Col<float>  MySimpleDoublePendulum::eval(const arma::Col<float> &x) const
 {
     const float cos_d = std::cos(x[0]-x[1]);
     const float sin_d = std::sin(x[0]-x[1]);
     const float den = 2 - cos_d*cos_d;
     const float den2 = den*den;
     const float prod = x[2]*x[3];
-    const float num = cos_d*sin_d*(x[2]*x[2] + 2*x[3]*x[3] + 2*prod);
+    const float num = sin_d*( (x[2]*x[2] + 2*x[3]*x[3])*cos_d - prod*(2+cos_d*cos_d));
+    const float f1 = cos_d / den;
+    const float f2 = num / den2;
 
-    return std::vector{
-        (x[2] - x[3]*cos_d)/den,
-        (2*x[3] - x[2]*cos_d)/den,
-        static_cast<float>(num / den2 + prod * sin_d / den - 2*M_G *std::sin(x[0])),
-        static_cast<float>(num / den2 - prod * sin_d / den - 2*M_G *std::sin(x[1]))
+    return arma::Col {
+        (-   x[2] + x[3]) * f1,
+        (- 2*x[3] + x[2]) * f1,
+        static_cast<float>(- f2 + 2*M_G *std::sin(x[0])),
+        static_cast<float>(  f2 +   M_G *std::sin(x[1]))
     };
 }
