8a4d828ceaf3288d9a6e103a4c385ff86603283a
Mattia04 <mattia.ballico@gmail.com>
2025-01-19 11:38:36 +0100

    fixed bug in calculations

--- source/MyDoublePendulum.cpp
+++ source/MyDoublePendulum.cpp
@@ -12,12 +12,11 @@ arma::Col<float>  MySimpleDoublePendulum::eval(const arma::Col<float> &x) const
     const float den2 = den*den;
     const float prod = x[2]*x[3];
     const float num = sin_d*( (x[2]*x[2] + 2*x[3]*x[3])*cos_d - prod*(2+cos_d*cos_d));
-    const float f1 = cos_d / den;
     const float f2 = num / den2;
 
     return arma::Col {
-        (-   x[2] + x[3]) * f1,
-        (- 2*x[3] + x[2]) * f1,
+        (-   x[2] + x[3]*cos_d) /den,
+        (- 2*x[3] + x[2]*cos_d) /den,
         static_cast<float>(- f2 + 2*M_G *std::sin(x[0])),
         static_cast<float>(  f2 +   M_G *std::sin(x[1]))
     };
