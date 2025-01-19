c948378136eb5ed18e083e43b954067209e89136
Mattia04 <mattia.ballico@gmail.com>
2025-01-19 18:55:02 +0100

    Added placeholder for Runge-Kutta-Fehlberg and Runge–Kutta-Dormand-Prince.

---
+++ include/MyRKDP45.h
@@ -0,0 +1,11 @@
+//
+// Created by Mattia Ballico on 19/01/25.
+//
+// Code for the Runge–Kutta_Dormand-Prince integrator
+// Global error: O(h^5) but with error estimate: O(h^4)
+//
+
+#ifndef MYRKDP45_H
+#define MYRKDP45_H
+
+#endif //MYRKDP45_H
---
+++ include/MyRKF45.h
@@ -0,0 +1,11 @@
+//
+// Created by Mattia Ballico on 19/01/25.
+//
+// Code for the Runge–Kutta–Fehlberg integrator
+// Global error: O(h^5) but with error estimate: O(h^4)
+//
+
+#ifndef MYRKF45_H
+#define MYRKF45_H
+
+#endif //MYRKF45_H
