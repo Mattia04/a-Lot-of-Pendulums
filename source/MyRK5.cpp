8de003e9997ff7bf81879db069e20129a7309214
Mattia04 <mattia.ballico@gmail.com>
2025-01-19 21:23:38 +0100

    made the butcher tableau a namespace + corrected RK5 and RK6, but RK7 and RK8 are not working.

--- source/MyRK5.cpp
+++ source/MyRK5.cpp
@@ -4,6 +4,26 @@
 
 #include "MyRK5.h"
 
+// Coefficients for the Butcher tableau
+namespace
+{
+	constexpr float m_A[5] = {
+		1./4, 1./4, 1./2, 3./4, 1.
+	}; // m_A is here just for future implementations with time dependence
+
+	constexpr float m_B[5][5] = {
+		{0.25},
+		{1./8, 1./8},
+		{0, -1./2, 1},
+		{3./16, 0, 0, 9./16},
+		{-3./7, 2./7, 12./7, -12./7, 8./7}
+	};
+
+	constexpr float m_C[6] = {
+		7./90, 0, 32./90, 12./90, 32./90, 7./90
+	};
+}
+
 void MyRK5::step(const float& t, const float& h, arma::Col<float>& x, MyFunctor& f) const {
 	arma::Col<float> k[6];
 
--- source/MyRK6.cpp
+++ source/MyRK6.cpp
@@ -4,6 +4,27 @@
 
 #include "MyRK6.h"
 
+namespace
+{
+	// Coefficients for the Butcher tableau for RK6
+	constexpr float m_A[6] = {
+		1./3, 2./3, 1./3, 1./2, 1./2, 1.
+	}; // m_A is here just for future implementations with time dependence
+
+	constexpr float m_B[6][6] = {
+		{1./3},
+		{0, 2./3},
+		{1./12, 1./3, -1./12},
+		{-1./16, 9./8, -3./16, -3./8},
+		{0, 9./8, -3./8, -3./4, 1./2},
+		{9./44, -9./11, 63./44, 18./11, 0, -16./11}
+	};
+
+	constexpr float m_C[7] = {
+		11./120, 0, 27./40, 27./40, -4./15, -4./15, 11./120
+	};
+}
+
 void MyRK6::step(const float& t, const float& h, arma::Col<float>& x, MyFunctor& f) const {
 	arma::Col<float> k[7];
 
