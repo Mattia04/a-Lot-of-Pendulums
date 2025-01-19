770be87a2f651e698f44da7d1d05fab05dd1af42
Mattia04 <mattia.ballico@gmail.com>
2025-01-19 18:54:19 +0100

    Added RK8.

---
+++ source/MyRK8.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Mattia Ballico on 19/01/25.
+//
+
+#include "MyRK8.h"
+
+void MyRK8::step(const float& t, const float& h, arma::Col<float>& x, MyFunctor& f) const {
+	arma::Col<float> k[9];
+
+	k[0] = h * f(x);
+	for (int i = 1; i < 9; i++) {
+		auto temp = x;
+		for (int j = 0; j < i; j++) {
+			temp += m_B[i-1][j] * k[j];
+		}
+		k[i] = h * f(temp);
+	}
+
+	for (int i = 0; i < 9; i++) {
+		x += m_C[i] * k[i];
+	}
+}
